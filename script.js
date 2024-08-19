976afcb4f1ded2164946aaea1ecdc7afdb5f265a
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-08-19 09:42:23 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+  get name() {
+    return this._name;
+  }
+  get age() {
+    return this._age;
+  }
+  set age(newage) {
+    this._age = newage;
+  }
+}
 
-class Student extends Person {}
+class Student extends Person {
+  study() {
+    console.log(this.name + " " + "is studying");
+  }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+  teach() {
+    console.log(this.name + " " + "is teaching");
+  }
+}
+const person = new Person("John", 25);
+console.log(person.name);
+
+person.age = 30;
+console.log(person.age);
+
+const student = new Student("Alice", 22);
+student.study();
+
+const teacher = new Teacher("Bob", 32);
+teacher.teach();
 
 // Do not change the code below this line
 window.Person = Person;
